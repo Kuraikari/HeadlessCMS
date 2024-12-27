7f81c12ed25234d58f343ed94b1c631b4e7af0e3
Zian Wermelinger <z.wermelinger@computech.ch>
2024-12-27 14:49:36 +0100

    Remove Dist

--- dist/frontend/src/app/layout.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = DashboardLayout;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function DashboardLayout({ children, }) {
-    return ((0, jsx_runtime_1.jsx)("html", { lang: "en", children: (0, jsx_runtime_1.jsx)("body", { children: (0, jsx_runtime_1.jsx)("main", { children: children }) }) }));
-}
--- dist/frontend/src/pages/App/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = App;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function App() {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("h2", { children: "My first Apollo app \uD83D\uDE80" }) }));
-}
